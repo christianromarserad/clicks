6a933161724a6b7e952a5a420f1bcdac8f69e367
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-05-01 17:51:24 -0400

    Added a layout component

--- chrome-extension/src/StyledComponents/StyledComponentLayout.js
+++ chrome-extension/src/StyledComponents/StyledComponentLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import theme from '../Configurations/theme';
+import { Flex } from './index';
 
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${(props) => props.theme.backgroundColor};
     color: ${(props) => props.theme.textColor};
+    font-family: ${(props) => props.theme.fontFamily};
     &, * {
       margin: 0;
       padding: 0;
@@ -19,7 +21,9 @@ function StyledComponentLayout({ children }) {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
-            {children}
+            <Flex mr="auto" ml="auto" vertical>
+                {children}
+            </Flex>
         </ThemeProvider>
     );
 }
