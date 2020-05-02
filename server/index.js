583291181b38973e1c255c22342c8b8b3c012962
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-05-02 14:49:44 -0400

    Setting up the required routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+//Setting up mongo db 
+mongoose.connect('mongodb://localhost/clicks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+let db = mongoose.connection;
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+db.on('error', (err) => {
+    console.log(err);
+});
 
 
-
-
-
-
+//Set up express
 const app = express();
 
+//middleware
+app.use(express.json());
 
+//routes
+app.use('/user', require('./routes/user'));
 
 
 
---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+const router = require('express').Router;
+
+router.post('/incrementCount', (req, res) => {
+   //TODO
+});
+
+
+router.get('getTop', (req, res) => {
+   //TODO
+})
+
+
+router.get('getDailyCounts', (req, res) => {
+   //TODO
+});
+
+module.exports = router;
