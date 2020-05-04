cfb3b80b53c3fa1d18c39805e117ce81e2bffdd8
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-05-04 12:44:05 -0400

    Click event handle for both local and server

--- chrome-extension/public/background.js
+++ chrome-extension/public/background.js
@@ -1,22 +1,69 @@
+function getCurrentDate() {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var currentDate = new Date();
+    let month = months[currentDate.getMonth()];
+    let day = currentDate.getDate();
+    let year = currentDate.getFullYear();
+
+    return `${month} ${day} ${year}`;
+}
 
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.local.set({ clickCount: 0 }, function () {
-        console.log('Count initialized to 0');
+    let initialUserInfo = {
+        totalCount: 0,
+        dailyClicks: [{
+            date: getCurrentDate(),
+            count: 0
+        }],
+    }
+    chrome.storage.local.set({ user: initialUserInfo }, function () {
+        console.log(initialUserInfo);
     });
 });
 
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
         if (request.event === 'click') {
-            chrome.storage.local.get(['clickCount'], function (result) {
-                if (result.clickCount === undefined) {
-                    chrome.storage.local.set({ clickCount: 0 }, function () {
-                        console.log('Count initialized to 0');
-                    });
+            chrome.identity.getAuthToken({ interactive: false }, function (token) {
+                if (token) {
+                    fetch('http://localhost:5000/user/incrementcount', {
+                        method: 'PUT',
+                        headers: {
+                            "Authorization": `Bearer ${token}`
+                        }
+                    }).catch((err) => {
+                        console.log(err);
+                    })
                 }
                 else {
-                    chrome.storage.local.set({ clickCount: result.clickCount + 1 }, function () {
-                        console.log(`Count is set to: ${result.clickCount + 1}`);
+                    chrome.storage.local.get(['user'], function (result) {
+                        let user = result.user;
+                        if (!user) {
+                            user = {
+                                totalCount: 1,
+                                dailyClicks: [{
+                                    date: getCurrentDate(),
+                                    count: 1
+                                }],
+                            };
+                        }
+                        else {
+                            user.totalCount++;
+                            let index = user.dailyClicks.findIndex((dailyClick) => (dailyClick.date === getCurrentDate()));
+                            if (index === -1) {
+                                user.dailyClicks.push({
+                                    date: getCurrentDate(),
+                                    count: 1
+                                });
+                            }
+                            else {
+                                user.dailyClicks[index].count++;
+                            }
+                        }
+
+                        chrome.storage.local.set({ user: user }, function () {
+                            console.log(user);
+                        });
                     });
                 }
             });
